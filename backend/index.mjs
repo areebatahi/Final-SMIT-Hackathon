63d7967097be2833ed01bc29fc8815d5c26075b4
areebatahi <areebatahirarain@gmail.com>
2025-04-20 02:39:31 +0500

    vercel hosted url

--- backend/index.mjs
+++ backend/index.mjs
@@ -10,7 +10,7 @@ const app = express();
 
 app.use(
 	cors({
-		origin: ['http://localhost:5174', 'http://localhost:5173'],
+		origin: ['http://localhost:5174', 'http://localhost:5173',"https://final-smit-hackathon-gamma.vercel.app"],
 		methods: ['GET', 'PUT', 'POST', 'DELETE'],
 		credentials: true,
 		allowedHeaders: ['Content-Type', 'Authorization'],
