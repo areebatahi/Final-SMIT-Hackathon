b1251985ab1a35a828733f14ad1b7b99714c511d
areebatahi <areebatahirarain@gmail.com>
2025-04-26 00:40:49 +0500

    fix error

--- backend/index.mjs
+++ backend/index.mjs
@@ -9,10 +9,16 @@ const app = express();
 
 app.use(
 	cors({
-		origin: ['http://localhost:5174', 'http://localhost:5173',"https://final-smit-hackathon-qc45qkl2w-areeba-tahirs-projects.vercel.app"],
-		methods: ['GET', 'PUT', 'POST', 'DELETE'],
-		credentials: true,
-		allowedHeaders: ['Content-Type', 'Authorization'],
+		origin: [
+			'http://localhost:5174',
+			'http://localhost:5173',
+			"https://final-smit-hackathon-qc45qkl2w-areeba-tahirs-projects.vercel.app/",
+			"https://final-smit-hackathon-production.up.railway.app",
+			/\.vercel\.app$/, // Wildcard for all Vercel subdomains
+			/\.up\.railway\.app$/, // Wildcard for all Railway subdomains],
+			methods: ['GET', 'PUT', 'POST', 'DELETE'],
+			credentials: true,
+			allowedHeaders: ['Content-Type', 'Authorization'],
 	}),
 );
 
@@ -20,13 +26,13 @@ app.use(
 app.use(express.json());
 const port = process.env.PORT || 5000;
 
-app.use("/api/auth",userRoutes)
+app.use("/api/auth", userRoutes)
 
 app.use("/", (req, res, next) => {
-  console.log("Request URL:", req.url, "method: ", req.method);
-  next();
+	console.log("Request URL:", req.url, "method: ", req.method);
+	next();
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+	console.log(`Example app listening on port ${port}`);
 });
