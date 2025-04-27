ab0bb20c617ce796687bb74fb2b4e206c982f7c1
areebatahi <areebatahirarain@gmail.com>
2025-04-27 12:01:09 -0700

    fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,39 +1,33 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route, useLocation } from 'react-router-dom';
 import LoginPage from './Pages/LoginPage';
 import SignUp from './Pages/Signup';
-import TaskManagement from './components/TaskManagement';
 import Logout from "./Pages/Logout";
+import TaskManagement from './components/TaskManagement';
 import Sidebar from './components/Slidebar';
 import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
-  return (
-    
-      <div className="flex">
-        <Sidebar />
-        <div className="flex-1 ml-64 min-h-screen bg-gray-50 p-4">
-          <ToastContainer
-            position="top-right"
-            autoClose={5000}
-            hideProgressBar={false}
-            newestOnTop={false}
-            closeOnClick
-            rtl={false}
-            pauseOnFocusLoss
-            draggable
-            pauseOnHover
-          />
-          <Routes>
-            <Route path="/" element={<LoginPage />} />
-            <Route path="/signup" element={<SignUp />} />
-            <Route path="/taskManagement" element={<TaskManagement />} />
-            <Route path="/logout" element={<Logout />} />
-          </Routes>
-        </div>
-      </div>
+  const location = useLocation();
 
+  const handleStatusChange = (statusText) => {
+    console.log("Status changed to:", statusText);
+  };
+
+  return (
+    <div className="flex">
+      <Sidebar onStatusChange={handleStatusChange} />
+      <div className="flex-1 ml-64 min-h-screen bg-gray-50 p-4">
+        {/* Move ToastContainer here if you want it inside the main content */}
+        <ToastContainer />
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/taskManagement" element={<TaskManagement />} />
+          <Route path="/logout" element={<Logout />} />
+        </Routes>
+      </div>
+    </div>
   );
 };
 
