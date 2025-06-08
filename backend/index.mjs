951f7f399ea01ea7d5425f82446decfff7898698
areebatahi <areebatahirarain@gmail.com>
2025-06-08 18:38:22 +0500

    .

--- backend/index.mjs
+++ backend/index.mjs
@@ -21,7 +21,7 @@ app.use(
       /\.vercel\.app$/,
       /\.up\.railway\.app$/,
     ],
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
   })
